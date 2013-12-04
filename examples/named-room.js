27218372755f3f8f9ddd3fa4fc10836b057140c4
Damon Oehlman <damon.oehlman@gmail.com>
2013-12-04 12:13:18 +1100

    Added named room example

---
+++ examples/named-room.js
@@ -0,0 +1,19 @@
+var quickconnect = require('../');
+var opts = {
+  room: 'dctest-room',
+  data: true,
+  signalhost: 'http://sig.rtc.io:50000'
+};
+
+quickconnect(opts)
+  .on('peer', function(connection, id, data, monitor) {
+    console.log('got a new friend: ' + id, connection);
+  })
+  .on('dc:open', function(dc, id) {
+    dc.addEventListener('message', function(evt) {
+      console.log('peer ' + id + ' says: ' + evt.data);
+    });
+
+    console.log('dc open for peer: ' + id);
+    dc.send('hi');
+  });
