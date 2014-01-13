974e65ab08cc6c231ab4e3665530b532b567b112
Damon Oehlman <damon.oehlman@gmail.com>
2014-01-13 14:03:00 +1100

    Restructured example, more flexible and not dependant on rtc-media

--- examples/conference.js
+++ examples/conference.js
@@ -1,28 +1,34 @@
 var quickconnect = require('../');
+var media = require('rtc-media');
 var crel = require('crel');
 
 // create containers for our local and remote video
 var local = crel('div', { class: 'local' });
 var remote = crel('div', { class: 'remote' });
+var media  
 var peerMedia = {};
 
-quickconnect('http://rtc.io/switchboard/', { ns: 'dctest' })
-  // add some media (use rtc-captureclass) to configure the stream
-  .addMedia('camera:0 min:1280x768', function(err, media) {
-    media.render(local);
-  })
-  // when a peer is connected (and active) pass it to us for use
-  .on('peer:connect', function(pc, id, data) {
-    // render the remote streams
-    pc.getRemoteStreams().forEach(renderRemote(id));
-  })
-  // when a peer leaves, remove teh media
-  .on('peer:leave', function(id) {
-    // remove media for the target peer from the dom
-    (peerMedia[id] || []).splice(0).forEach(function(el) {
-      el.parentNode.removeChild(el);
-    });
-  })
+// capture local media
+var localMedia = media();
+
+// once media is captured, connect
+localMedia.once('capture', function(stream) {
+  quickconnect('http://rtc.io/switchboard/', { ns: 'dctest' })
+    // broadcast our captured media to other participants in the room
+    .broadcast(stream)
+    // when a peer is connected (and active) pass it to us for use
+    .on('peer:connect', function(pc, id, data) {
+      // render the remote streams
+      pc.getRemoteStreams().forEach(renderRemote(id));
+    })
+    // when a peer leaves, remove teh media
+    .on('peer:leave', function(id) {
+      // remove media for the target peer from the dom
+      (peerMedia[id] || []).splice(0).forEach(function(el) {
+        el.parentNode.removeChild(el);
+      });
+    })
+});
 
 // render a remote video
 function renderRemote(id) {
