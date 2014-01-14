c5b5a69af36f9c6c09b1159ada36eaa91fc4964e
Damon Oehlman <damon.oehlman@gmail.com>
2014-01-14 11:02:17 +1100

    Fix simple example

--- examples/simple.js
+++ examples/simple.js
@@ -2,5 +2,5 @@ var quickconnect = require('../');
 
 quickconnect('http://rtc.io/switchboard/')
   .on('peer', function(pc, id, data, monitor) {
-    console.log('got a new friend, id: ' + id, conn);
+    console.log('got a new friend, id: ' + id, pc);
   });
