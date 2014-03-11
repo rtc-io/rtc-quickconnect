2c604630deb0c5f5a4befd08735c7795ad2480b3
Damon Oehlman <damon.oehlman@gmail.com>
2014-03-11 12:53:47 +1100

    Tweak example to use a configured channel name

--- examples/ice-configuration.js
+++ examples/ice-configuration.js
@@ -8,9 +8,9 @@ var opts = {
 
 quickconnect('http://rtc.io/switchboard/', opts)
   // tell quickconnect we want a datachannel called test
-  .createDataChannel('test')
+  .createDataChannel('iceconfig')
   // when the test channel is open, let us know
-  .on('test:open', function(dc, id) {
+  .on('iceconfig:open', function(dc, id) {
     dc.onmessage = function(evt) {
       console.log('peer ' + id + ' says: ' + evt.data);
     };
