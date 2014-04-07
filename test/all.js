fd211da6c52debdf969addda253f263614a74c35
Damon Oehlman <damon.oehlman@gmail.com>
2014-04-07 22:16:16 +1000

    Include load tests in all tests

--- test/all.js
+++ test/all.js
@@ -2,3 +2,6 @@ require('./profile');
 require('./datachannel');
 // require('./heartbeat-disconnect');
 require('./custom-id');
+
+// include the simple load test
+require('./all-loadtest');
--- test/loadtest-peerconnection.js
+++ test/loadtest-peerconnection.js
@@ -8,22 +8,12 @@ function addNew(t) {
   var conn;
   var expectedPeers = connections.length;
 
-  t.plan(2);
-
-  // initialise the connection
+  t.plan(1);
   conn = quickconnect(location.origin, { room: roomId });
-
-  if (connections.length === 0) {
-    t.pass('no peer channels yet available');
-  }
-  else {
-    async.parallel(connections.map(function(conn) {
-      return conn.once.bind(conn, 'dc:open');
-    }), t.pass.bind(t, 'peer channels open'));
-  }
 
   // create the new data channel
   conn.createDataChannel('dc');
+  connections.push(conn);
 
   if (expectedPeers === 0) {
     t.pass('no peers expected');
@@ -32,27 +22,29 @@ function addNew(t) {
     conn.on('dc:open', function handleChannelOpen() {
       expectedPeers -= 1;
       if (expectedPeers <= 0) {
-        t.pass('got all expected peers');
+        t.pass('all expected data channels opened');
+        conn.removeListener('dc:open', handleChannelOpen);
+
+        // release the connection reference
+        conn = null;
       }
 
-      conn.removeListener('dc:open', handleChannelOpen);
+
     });
   }
 }
 
-for (var ii = 0, count = 20; ii < count; ii++) {
-  test('create ' + (ii + 1), addNew);
+// create 6 connections with a full-way mesh (30 peer connections total)
+for (var ii = 0, count = 6; ii < count; ii++) {
+  test('load test: create ' + (ii + 1), addNew);
 }
 
 test('close the connections', function(t) {
   t.plan(1);
 
-  connections.forEach(function(conn) {
+  connections.splice(0).forEach(function(conn) {
     conn.close();
   });
-
-  // reset the connections array
-  connections = [];
 
   t.pass('connections closed');
 });
