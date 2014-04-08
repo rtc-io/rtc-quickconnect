ce367e72edbd325df2fc085458ecbb64b6a50e59
Damon Oehlman <damon.oehlman@gmail.com>
2014-04-08 21:00:30 +1000

    Update chrome start flags

--- test/scripts/run-chrome.sh
+++ test/scripts/run-chrome.sh
@@ -2,4 +2,4 @@
 DEBUG_OPTS="--enable-logging --v=1 --vmodule=*third_party/libjingle/*=3,*=0"
 
 rm -rf $HOME/.config/chrome-test
-google-chrome --console --no-first-run --user-data-dir=$HOME/.config/chrome-test --use-fake-device-for-media-stream --use-fake-ui-for-media-stream $@
+google-chrome --console --no-first-run --no-sandbox --disable-setuid-sandbox --allow-sandbox-debugging --user-data-dir=$HOME/.config/chrome-test --use-fake-device-for-media-stream --use-fake-ui-for-media-stream $@
