5fb968d99d7f63865aeb18f452ec9bc52bf59248
Damon Oehlman <damon.oehlman@gmail.com>
2014-04-08 17:30:41 +1000

    Added chrome runner for testling testing

---
+++ test/run-chrome.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+DEBUG_OPTS="--enable-logging --v=1 --vmodule=*third_party/libjingle/*=3,*=0"
+
+rm -rf $HOME/.config/chrome-test
+google-chrome --console --no-first-run --user-data-dir=$HOME/.config/chrome-test --use-fake-device-for-media-stream $@
