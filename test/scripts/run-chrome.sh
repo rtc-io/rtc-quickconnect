75af2e90a35469f6682c34f4e06510990669924e
Damon Oehlman <damon.oehlman@gmail.com>
2014-04-08 21:25:40 +1000

    Tweak chrome run script

--- test/scripts/run-chrome.sh
+++ test/scripts/run-chrome.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 DEBUG_OPTS="--enable-logging --v=1 --vmodule=*third_party/libjingle/*=3,*=0"
+EXTRA_OPTS="--no-sandbox --disable-setuid-sandbox --allow-sandbox-debugging"
 
-rm -rf $HOME/.config/chrome-test
-google-chrome --console --no-first-run --no-sandbox --disable-setuid-sandbox --allow-sandbox-debugging --user-data-dir=$HOME/.config/chrome-test --use-fake-device-for-media-stream --use-fake-ui-for-media-stream $@
+google-chrome \
+  --console \
+  --no-first-run \
+  --use-fake-device-for-media-stream \
+  --use-fake-ui-for-media-stream $@ \
+  --user-data-dir
