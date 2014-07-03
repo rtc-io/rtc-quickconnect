11806a78820156aba3614add427fc3ae229b8d0f
Nathan Oehlman <nathan.oehlman@sidelab.com>
2014-07-03 15:13:43 +1000

    Change connection:create to peer:connect, and add peer:couple event for monitoring purposes

--- index.js
+++ index.js
@@ -368,7 +368,7 @@ module.exports = function(signalhost, opts) {
 
     // create a peer connection
     pc = rtc.createConnection(opts, (opts || {}).constraints);
-    signaller.emit('connection:create', pc, data);
+    signaller.emit('peer:connect', pc, data);
 
     // add this connection to the calls list
     callCreate(data.id, pc, data);
@@ -407,6 +407,7 @@ module.exports = function(signalhost, opts) {
     // couple the connections
     debug('coupling ' + signaller.id + ' to ' + data.id);
     monitor = rtc.couple(pc, data.id, signaller, opts);
+    signaller.emit('peer:couple', pc, data, monitor);
 
     // once active, trigger the peer connect event
     monitor.once('connected', callStart.bind(null, data.id, pc, data))
