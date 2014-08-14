794a671862448a52c437b8a75c4c29bebdbae228
Damon Oehlman <damon.oehlman@gmail.com>
2014-08-14 13:40:20 +1000

    Remove the reconnection tests from the firefox testsuite

--- test/all.js
+++ test/all.js
@@ -15,10 +15,14 @@ require('./datachannel');
 // require('./heartbeat-disconnect');
 require('./custom-id');
 require('./request-stream');
-require('./reconnect');
 require('./events');
 
+// we need some firefox issues resolved before all tests can be run
 if (! detect.moz) {
+  // https://bugzilla.mozilla.org/show_bug.cgi?id=852665
+  require('./reconnect');
+
+  // https://bugzilla.mozilla.org/show_bug.cgi?id=857115
   require('./reactive');
   require('./reactive-stream-events');
 }
