88a73975b221b0002d1ee4d5558e2a49e6eabde3
Damon Oehlman <damon.oehlman@gmail.com>
2014-09-01 10:32:43 +1000

    Additional test helpers

---
+++ test/helpers/signaling-server.js
@@ -0,0 +1,1 @@
+module.exports = typeof window != 'undefined' ? location.origin : 'http://localhost:3000';
---
+++ test/helpers/stun-google.js
@@ -0,0 +1,6 @@
+module.exports = [
+  { url: 'stun:stun1.l.google.com:19302' },
+  { url: 'stun:stun2.l.google.com:19302' },
+  { url: 'stun:stun3.l.google.com:19302' },
+  { url: 'stun:stun4.l.google.com:19302' }
+];
