f69fe662235d6c82ea95a0f6e156822095a0e86f
Damon Oehlman <damon.oehlman@gmail.com>
2014-10-28 15:26:17 +1100

    Refactor multiparty test to match multiparty reactive test

--- test/multiparty.js
+++ test/multiparty.js
@@ -4,14 +4,55 @@ var uuid = require('uuid');
 var connections = [];
 var dcs = [];
 var roomId = uuid.v4();
-var addConnection = require('./helpers/add-connection');
+var connectionCount = 4;
+var times = require('whisk/times');
+var pluck = require('whisk/pluck');
 
 // require('cog/logger').enable('rtc-quickconnect');
 
-test('connect 0', addConnection(roomId, connections));
-test('connect 1', addConnection(roomId, connections));
-test('connect 2', addConnection(roomId, connections));
-test('connect 3', addConnection(roomId, connections));
+test('create ' + connectionCount + ' connections', function(t) {
+  connections = times(connectionCount).map(function() {
+    var qc = quickconnect(location.origin, {
+      room: roomId,
+      iceServers: require('./helpers/stun-google')
+    });
+
+    // create a single data channel
+    qc.createDataChannel('test');
+    return qc;
+  });
+
+  t.plan(connections.length);
+  connections.forEach(function(conn) {
+    conn.once('connected', t.pass.bind(t, 'connected'));
+  });
+});
+
+
+test('establish connection matrix', function(t) {
+  t.plan(connections.length * (connections.length - 1));
+
+  connections.forEach(function(conn) {
+    var expected = connections.map(pluck('id')).filter(function(id) {
+      return id !== conn.id;
+    });
+
+    function callStart(id) {
+      var idx = expected.indexOf(id);
+
+      t.ok(idx >= 0, conn.id + ' started call with ' + id);
+      if (idx >= 0) {
+        expected.splice(idx, 1);
+      }
+
+      if (expected.length === 0) {
+        conn.removeListener('call:started', callStart);
+      }
+    }
+
+    conn.on('call:started', callStart);
+  });
+});
 
 test('clean up', function(t) {
   t.plan(1);
