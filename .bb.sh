74c50e5d8b898abc095774cdd64a37b493ef7419
Damon Oehlman <damon.oehlman@gmail.com>
2014-11-11 16:47:08 +1100

    Refine the buildbox script

--- .bb.sh
+++ .bb.sh
@@ -1,1 +1,11 @@
-npm install && npm test
+set -e
+
+# bootstrap the build
+mkdir -p .travis
+curl -s https://codeload.github.com/rtc-io/webrtc-testing-on-travis/tar.gz/master | tar -xz --strip-components=1 --directory .travis
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start
+
+# install deps and run the test command
+npm install
+npm test
