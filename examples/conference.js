6228197fc7a0dd900cbf350b727be6fa061cfde3
Damon Oehlman <damon.oehlman@gmail.com>
2014-12-18 17:04:11 +1100

    Refactor conference example to use getusermeda and attachmediastream packages

--- examples/conference.js
+++ examples/conference.js
@@ -1,51 +1,42 @@
 var quickconnect = require('../');
-var media = require('rtc-media');
 var crel = require('crel');
+var getUserMedia = require('getusermedia');
+var attachmediastream = require('attachmediastream');
+var qsa = require('fdom/qsa');
 
 // create containers for our local and remote video
 var local = crel('div', { class: 'local' });
 var remote = crel('div', { class: 'remote' });
-var media  
 var peerMedia = {};
 
-// capture local media
-var localMedia = media();
-
-// require('cog/logger').enable('*');
-
 // once media is captured, connect
-localMedia.once('capture', function(stream) {
-  quickconnect('http://rtc.io/switchboard/', { room: 'conftest' })
+getUserMedia({ audio: true, video: true }, function(err, localStream) {
+  if (err) {
+    return console.error('could not capture media: ', err);
+  }
+
+  // render the local media
+  local.appendChild(attachmediastream(localStream));
+
+  // initiate connection
+  quickconnect('https://switchboard.rtc.io/', { room: 'conftest' })
     // broadcast our captured media to other participants in the room
-    .addStream(stream)
+    .addStream(localStream)
     // when a peer is connected (and active) pass it to us for use
     .on('call:started', function(id, pc, data) {
-      console.log('peer connected: ', id);
-
-      // render the remote streams
-      pc.getRemoteStreams().forEach(renderRemote(id));
+      var videos = pc.getRemoteStreams().map(attachmediastream);
+      videos.forEach(function(video) {
+        video.dataset.peer = id;
+        remote.appendChild(video);
+      });
     })
     // when a peer leaves, remove teh media
     .on('call:ended', function(id) {
-      // remove media for the target peer from the dom
-      (peerMedia[id] || []).splice(0).forEach(function(el) {
+      qsa('*[data-peer="' + id + '"]', remote).forEach(function(el) {
         el.parentNode.removeChild(el);
       });
-    })
+    });
 });
-
-// render the local media
-localMedia.render(local);
-
-// render a remote video
-function renderRemote(id) {
-  // create the peer media list
-  peerMedia[id] = peerMedia[id] || [];
-
-  return function(stream) {
-    peerMedia[id] = peerMedia[id].concat(media(stream).render(remote));
-  }
-}
 
 /* extra code to handle dynamic html and css creation */
 
