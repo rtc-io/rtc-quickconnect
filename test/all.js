257326578e4e18e290f801f2eab6fd433d19d1c5
Damon Oehlman <damon.oehlman@gmail.com>
2015-04-29 17:02:33 +1000

    Ensure we prime the test signaller using both / and /primus endpoints

--- test/all.js
+++ test/all.js
@@ -2,7 +2,10 @@ var signaller = require('rtc-pluggable-signaller');
 var extend = require('cog/extend');
 
 function createSignaller(opts) {
-  return signaller(extend({ signaller: location.origin }, opts));
+  return signaller(extend({
+    signaller: location.origin,
+    endpoints: ['/', '/primus']
+  }, opts));
 }
 
 require('rtc-quickconnect-test')(
